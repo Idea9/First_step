c2c865327db8ab253a2812e71dc5cb3d1f71e239
Idea9 <puzikfx@gmail.com>
2015-11-10 09:53:27 +0100

    First program

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Witaj Inżynieże")
+
+pracuje = input("Napisz jak pracujemy nad inżynierką: ")
+
+if pracuje == "na kurwie":
+    print("no coś z tego będzie")
+else:
+    print("mocno średnio to widze")
